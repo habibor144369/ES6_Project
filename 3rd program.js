af1900a48b8dd32649d8254cbbf75dba1d74494c
Your Name <you@example.com>
2020-07-05 15:00:15 +0600

    this my computer

---
+++ 3rd program.js
@@ -0,0 +1,23 @@
+// string add 1st methode of program....
+// const firstName = "justin";
+// const lastName = "timberlack";
+// const fullName = firstName + " " + lastName + " is a good boy";
+// console.log(fullName);
+
+// string add 2nd methode of program...
+const firstName = "justin";
+const lastName = "timberlack";
+const fullName = `${firstName} ${lastName} ${70 + 80 + 50 -100} is a good boy. and very smart person.`;
+console.log(fullName);
+
+//old multiLine write method.....
+const multiLine = "i am agood boy.\n" 
+ + "im a student and web developper.\n"
+ + "i love my allah!"
+ console.log(multiLine);
+
+ //new methode ES6 multiLine write...
+ const multiLine2 = `This is my computer.
+i love to play foot ball game fifa 11.
+it's vary intresting game of computer,`;
+ console.log(multiLine2);
