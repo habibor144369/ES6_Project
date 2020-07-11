c8ab66c705872de55fa210e50f9aba51d2d51ba2
Your Name <you@example.com>
2020-07-11 22:02:17 +0600

    this add

---
+++ 8th_program.js
@@ -0,0 +1,8 @@
+
+// function diclearetion in variable.....
+const  doubleIt = function myFunction(number){
+    const add = number * 2;
+     return add;
+ }
+ const result = doubleIt(10);
+ console.log(result);
